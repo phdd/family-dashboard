d94abbe7ecba9b1424223067289b406af0398858
Peter Heisig <peter@heisig.rocks>
2025-01-14 09:16:59 +0000

    use PWA update stuff

--- app.vue
+++ app.vue
@@ -1,8 +1,17 @@
 <script lang="ts" setup>
 // import '@ionic/vue/css/palettes/dark.always.css';
+const { $pwa } = useNuxtApp()
+
+watchEffect(() => {
+  if ($pwa?.needRefresh) {
+    $pwa.updateServiceWorker();
+  }
+});
 </script>
 
 <template>
+  <NuxtPwaManifest />
+  <NuxtLoadingIndicator />
   <ion-app>
     <ion-router-outlet />
   </ion-app>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/ionic', '@nuxtjs/tailwindcss', 'nuxt-particles'],
+  modules: [
+    '@nuxtjs/ionic',
+    '@nuxtjs/tailwindcss',
+    'nuxt-particles',
+    '@vite-pwa/nuxt'
+  ],
   css: ['~/assets/ionic.scss', '~/assets/style.scss'],
-  ssr: false
+  ssr: false,
+  pwa: {
+    client: {
+      periodicSyncForUpdates: 60,
+    },
+    manifest: {
+      name: 'Family Dashboard',
+      short_name: 'Dasboard',
+      theme_color: '#ffffff',
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  }
 })
