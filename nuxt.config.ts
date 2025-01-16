5b34e9fedafe60616b68a53ddf13be9e31678cae
Peter Heisig <peter@heisig.rocks>
2025-01-16 07:34:42 +0000

    fix: navigate fallback removed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,9 +21,6 @@ export default defineNuxtConfig({
     },
     devOptions: {
       enabled: true,
-      suppressWarnings: true,
-      navigateFallback: '/',
-      navigateFallbackAllowlist: [/^\/$/],
       type: 'module',
     },
   }
